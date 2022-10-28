298b0d135d417a8361453c3ce14a04bb4eefed27
Arturo Bernal <arturober@gmail.com>
2022-10-28 16:35:38 +0200

    main

--- Ejercicio POO1/src/App.java
+++ Ejercicio POO1/src/App.java
@@ -1,9 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void apartado1() {
         Jugador j1 = new Jugador("Juanito", 34, 40000);
         Jugador j2 = new Jugador("Pepito", 26, 32000);
 
         System.out.println(j1);
         System.out.println(j2);
+    }
+
+    public static void main(String[] args) throws Exception {
+        apartado1();
     }
 }
