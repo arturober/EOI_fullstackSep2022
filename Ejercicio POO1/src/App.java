5154f27419936f71fbfd555288a9107baafbc991
Arturo Bernal <arturober@gmail.com>
2022-10-28 17:27:10 +0200

    apartado 2

--- Ejercicio POO1/src/App.java
+++ Ejercicio POO1/src/App.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void apartado1() {
         Jugador j1 = new Jugador("Juanito", 34, 40000);
@@ -7,7 +10,30 @@ public class App {
         System.out.println(j2);
     }
 
+    public static void apartado2() {
+        List<Jugador> jugadores = new ArrayList<Jugador>();
+
+        Jugador a = new Jugador("Víctor", 25, 400);
+        Jugador b = new Jugador("Javi", -26, -500);
+        Jugador c = new Jugador("Alejandro", 34, 800);
+        Jugador d = new Jugador("Diego", 31, 950);
+
+        jugadores.add(a);
+        jugadores.add(b);
+        jugadores.add(null);
+        jugadores.add(c);
+        jugadores.add(null);
+        jugadores.add(d);
+
+        for(Jugador jugador : jugadores) { 
+            if (jugador!=null) {
+                System.out.println(jugador.toString()); 
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        apartado1();
+        // apartado1();
+        apartado2();
     }
 }
