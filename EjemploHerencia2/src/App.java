b6cc3502f2fba08368132bc592d901bda4bb07f6
Arturo Bernal <arturober@gmail.com>
2022-10-31 18:40:44 +0100

    polimorfismo

--- EjemploHerencia2/src/App.java
+++ EjemploHerencia2/src/App.java
@@ -1,3 +1,8 @@
+import java.rmi.server.ObjID;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void ejemplo1() {
         // Este ejemplo ya no funciona al ser la clase Animal abstracta (abstract)
@@ -50,11 +55,67 @@ public class App {
         p.hablar();
     }
 
+    // Polimorfismo
+    public static void ejemplo6() {
+        Object o = "Hola";
+        Object o2 = LocalDate.now();
+        Object o3 = new Ave("Pájaro", 1, true);
+        Object o4 = new Persona("Juan");
+
+        System.out.println(o2.toString()); 
+        System.out.println(o3.toString());
+
+        // o3.comer(); // Error. No puedo acceder al método comer
+
+        List<Object> lista = new ArrayList<>();
+        lista.add(34);
+        lista.add("Hola");
+        lista.add(LocalDate.now());
+        lista.add(new Mamifero("Leon", 300, true));
+
+        for(Object item: lista) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public static void ejemplo7() {
+        List<Animal> animales = new ArrayList<>();
+        animales.add(new Mamifero("Gacela", 80, false));
+        animales.add(new Ave("Loro", 1, true));
+        animales.add(new Mamifero("Leon", 300, true));
+        animales.add(new Ave("Avestruz", 50, false));
+
+        for (Animal animal : animales) {
+            System.out.println("-------------- " + animal.tipoAnimal() + " --------------");
+            System.out.println(animal.toString());
+            animal.comer();
+            if(animal instanceof Ave) { // Objeto de la clase Ave o derivada
+                Ave ave = (Ave)animal;
+                ave.ponerHuevos();
+                // ((Ave)animal).ponerHuevos(); // También se puede hacer en una línea
+            }
+        }
+    }
+
+    public static void ejemplo8() {
+        List<IHablador> habladores = new ArrayList<>();
+        habladores.add(new Ave("Loro", 1, true));
+        habladores.add(new Persona("Pepe"));
+
+        for (IHablador hablador : habladores) {
+            hablador.hablar();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         // ejemplo1();
         // ejemplo2();
         // ejemplo3();
         // ejemplo4();
-        ejemplo5();
+        // ejemplo5();
+
+        // ejemplo6();
+        // ejemplo7();
+        ejemplo8();
     }
 }
