2fe939f12b6d9cef0c61505558179e7b1085a473
Arturo Bernal <arturober@gmail.com>
2022-11-02 17:24:41 +0100

    ejemplos listas lambda

--- EjemploColecciones1/src/App.java
+++ EjemploColecciones1/src/App.java
@@ -5,13 +5,14 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class App {
     /**
      * Transformar array a lista
      */
     public static void ejemplo1() {
-        Integer[] numeros = { 3, 4, 5, 6, 2};
+        Integer[] numeros = { 3, 4, 5, 6, 2 };
         Arrays.sort(numeros);
         Arrays.toString(numeros);
         List<Integer> list = new ArrayList<>(List.of(numeros));
@@ -25,7 +26,7 @@ public class App {
         System.out.println(numeros);
         numeros.add(0, 99);
         System.out.println(numeros);
-        
+
         numeros.addAll(List.of(10, 11, 12, 13));
         System.out.println(numeros);
     }
@@ -55,7 +56,8 @@ public class App {
         System.out.println(numeros.indexOf(12));
         System.out.println(numeros.containsAll(List.of(5, 6, 3)));
 
-        // Cuidado con las listas de Integer (por defecto remove cuando recibe un número borra la posición)
+        // Cuidado con las listas de Integer (por defecto remove cuando recibe un número
+        // borra la posición)
         numeros.remove(3); // Borra el índice 3 (valor = 6)
         // Tendremos que crear un objeto Integer para que borre por valor
         numeros.remove(Integer.valueOf(3)); // Borra el valor 3 (índice = 0)
@@ -68,7 +70,7 @@ public class App {
         // Método 1 (Collections.sort): Ordenar por defecto listas
         // Collections.sort(numeros);
 
-         // Método 2 (List.sort) (Hay que pasarle un comparador o null por defecto)
+        // Método 2 (List.sort) (Hay que pasarle un comparador o null por defecto)
         numeros.sort(null);
         System.out.println(numeros);
 
@@ -80,13 +82,78 @@ public class App {
         System.out.println(numeros);
     }
 
+    // Métodos que reciben funciones (lambdas)
+    public static void ejemplo6() {
+        List<Integer> numeros = new ArrayList<>(List.of(3, 2, 1, 7, 6, 8));
+        System.out.println(numeros);
 
+        // Borrar números pares
+        numeros.removeIf(n -> n % 2 == 0);
+        System.out.println(numeros);
+
+        numeros.replaceAll(n -> n * 2);
+        System.out.println(numeros);
+    }
+
+    /**
+     * Ordenar una lista en base a una función de comparación (lambda)
+     */
+    public static void ejemplo7() {
+        List<String> palabras = new ArrayList<>(
+            List.of("gato", "perro", "aro", "murciélago", "patata")
+        );
+        palabras.sort(null);
+        System.out.println(palabras);
+
+        // Ordenar las palabras por su longitud
+        palabras.sort((p1, p2) -> p1.length() - p2.length());
+        System.out.println(palabras); // [aro, gato, perro, patata, murciélago]
+        // Al revés
+        palabras.sort((p1, p2) -> p2.length() - p1.length());
+        System.out.println(palabras); // [murciélago, patata, perro, gato, aro]
+        // Otra forma de comparar números
+        palabras.sort((p1, p2) -> Integer.compare(p1.length(), p2.length()));
+        System.out.println(palabras); // [aro, gato, perro, patata, murciélago]
+    }
+
+    // Ordenar objetos
+    public static void ejemplo8() {
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(24, "Juan"));
+        personas.add(new Persona(19, "Ana"));
+        personas.add(new Persona(43, "Marta"));
+        personas.add(new Persona(33, "Zacarías"));
+        personas.add(new Persona(17, "Paula"));
+
+        System.out.println(personas);
+
+        // Ordenar por edad
+        personas.sort((p1, p2) -> p1.getEdad() - p2.getEdad());
+        System.out.println(personas);
+
+        // Ordenar por nombre
+        personas.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+        System.out.println(personas);
+
+        // Ordenar por edad 2 (Comparing)
+        personas.sort(Comparator.comparing(p -> p.getEdad()));
+        System.out.println(personas);
+
+        // Ordenar por edad 3 (Comparing)
+        personas.sort(Comparator.comparing(Persona::getEdad));
+
+        personas.forEach(p -> System.out.println(p));
+        // personas.forEach(System.out::println); // Igual que lo anterior
+    }
 
     public static void main(String[] args) throws Exception {
         // ejemplo1();
         // ejemplo2();
         // ejemplo3();
         // ejemplo4();
-        ejemplo5();
+        // ejemplo5();
+        // ejemplo6();
+        // ejemplo7();
+        ejemplo8();
     }
 }
---
+++ EjemploColecciones1/src/Persona.java
@@ -0,0 +1,30 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    
+    public Persona(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + edad + ")";
+    }
+}
