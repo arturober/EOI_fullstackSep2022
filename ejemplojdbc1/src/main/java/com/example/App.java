6ede56ca3db55871ee3eec0f7e8ad3592e57ede3
Arturo Bernal <arturober@gmail.com>
2022-11-04 15:35:43 +0100

    ejemplo select jdbc

--- ejemplojdbc1/src/main/java/com/example/App.java
+++ ejemplojdbc1/src/main/java/com/example/App.java
@@ -2,8 +2,9 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.sql.Statement;
 
 /**
  * Hello world!
@@ -14,11 +15,21 @@ public class App {
     public final static String USER = "root";
     public final static String PASS = "";
 
-    public static void main(String[] args) {
+    public static void ejemplo1() {
         try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
-            System.out.println("Conexión establecida");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM category");
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String nombre = rs.getString("name");
+                System.out.println(id + " - " + nombre);
+            }
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        ejemplo1();
     }
 }
