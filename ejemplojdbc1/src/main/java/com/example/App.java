09ad9d76670efc8826089c1529c462253d500e0d
Arturo Bernal <arturober@gmail.com>
2022-11-04 17:24:25 +0100

    Ejemplo inyección SQL

--- ejemplojdbc1/src/main/java/com/example/App.java
+++ ejemplojdbc1/src/main/java/com/example/App.java
@@ -7,19 +7,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * Hello world!
- *
+ * Consultas SELECT con Statement
  */
 public class App {
     public final static String CADENA_CONEXION = "jdbc:mariadb://localhost:3306/product-manager";
     public final static String USER = "root";
     public final static String PASS = "";
 
+    /**
+     * Listar todas las categorías
+     */
     public static void ejemplo1() {
         try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM category");
-            while(rs.next()) {
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String nombre = rs.getString("name");
                 System.out.println(id + " - " + nombre);
@@ -29,7 +31,80 @@ public class App {
         }
     }
 
+    /**
+     * Listar categorías desde el final hacia atrás
+     */
+    public static void ejemplo2() {
+        try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = st.executeQuery("SELECT * FROM category");
+            rs.afterLast();
+            while (rs.previous()) {
+                int id = rs.getInt("id");
+                String nombre = rs.getString("name");
+                System.out.println(id + " - " + nombre);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Cambiar el nombre a la última categoría por "Laptops"
+     */
+    public static void ejemplo3() {
+        try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = st.executeQuery("SELECT * FROM category");
+            rs.last();
+            rs.updateString("name", "Laptops");
+            int id = rs.getInt("id");
+            rs.updateRow();
+            System.out.println("Categoría " + id + " actualizada");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+    
+    /**
+     * Insertar una nueva categoría a partir de un resultSet
+     */
+    public static void ejemplo4() {
+        try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = st.executeQuery("SELECT * FROM category");
+            rs.moveToInsertRow();
+            rs.updateString("name", "New category"); // La id no hace falta (autonumérica)
+            rs.insertRow();
+            rs.last(); // VAmos a la fila insertada
+            System.out.println("Categoría insertada con id " + rs.getInt("id"));
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Borrar la última categoría
+    */
+    public static void ejemplo5() {
+        try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = st.executeQuery("SELECT * FROM category");
+            rs.last();
+            int id = rs.getInt("id");
+            rs.deleteRow();
+            System.out.println("Categoría " + id + " borrada");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        ejemplo1();
+        // ejemplo1();
+        // System.out.println(" ---------------------- ");
+        // ejemplo2();
+        // ejemplo3();
+        // ejemplo4();
+        // ejemplo5();
     }
 }
---
+++ ejemplojdbc2/src/main/java/com/example/App.java
@@ -0,0 +1,78 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public final static String PRODUCT_MANAGER = "jdbc:mariadb://localhost:3306/product-manager";
+    public final static String INSTITUTO = "jdbc:mariadb://localhost:3306/instituto";
+    public final static String USER = "root";
+    public final static String PASS = "";
+
+    /**
+     * Mostrar productos con la referencia que indique el usuario MAL
+     * Probad lo siguiente como referencia: ' OR name LIKE '% 
+     * Muestra todos los productos
+     */
+    public static void ejemplo1() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Referencia del producto a buscar: ");
+        String referencia = sc.nextLine();
+        String sql = "SELECT * FROM product WHERE reference = '" + referencia + "'";
+        System.out.println("Ejecutando: " + sql);
+
+        try (Connection conn = DriverManager.getConnection(PRODUCT_MANAGER, USER, PASS)) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String ref = rs.getString("reference");
+                String nombre = rs.getString("name");
+                double precio = rs.getDouble("price");
+                System.out.printf("%3d - %8s %20s %10.2f\n", id, ref, nombre, precio);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Mostrar datos de las asignaturas que contengan una cadena en el nombre
+     * Probad con esto como nombre (con un espacio al final):
+     * ' UNION SELECT fecha, nota, asignatura FROM examen WHERE alumno IN (SELECT nia FROM alumno WHERE nombre = 'Juan'); -- 
+     * Debería sacar también las notas del alumno "Pepe"
+     */
+    public static void ejemplo2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe parte del nombre de la asignatura: ");
+        String nombreBuscar = sc.nextLine();
+        String sql = "SELECT * FROM asignatura WHERE nombre LIKE '%" + nombreBuscar + "%'";
+        System.out.println("Ejecutando: " + sql);
+
+        try (Connection conn = DriverManager.getConnection(INSTITUTO, USER, PASS)) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                String codigo = rs.getString("codigo");
+                String nombre = rs.getString("nombre");
+                String curso = rs.getString("curso");
+                System.out.printf("%-8s %20s %6s\n", codigo, nombre, curso);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public static void main( String[] args ) {
+        // ejemplo1();
+        ejemplo2();
+    }
+}
