688e35918f2f5183cbcef55cfe47359d4033f598
Arturo Bernal <arturober@gmail.com>
2022-11-04 18:47:59 +0100

    solución apartado 2

--- ejerciciosjdbc/src/main/java/com/example/App.java
+++ ejerciciosjdbc/src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,7 +16,7 @@ public class App {
         try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM categorias");
-            while(rs.next()) {
+            while (rs.next()) {
                 int categoria = rs.getInt("categoria");
                 String titulo = rs.getString("titulo");
                 int salario = rs.getInt("salario");
@@ -27,7 +28,27 @@ public class App {
         }
     }
 
-    public static void main( String[] args ) {
-        apartado1();
+    public static void apartado2() {
+        int edad = 40;
+        String contrato = "1998-01-01";
+        String sql = "SELECT * FROM empleados WHERE edad >= ? AND contrato < ?";
+
+        try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, edad);
+            st.setString(2, contrato);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt("num") + " - " + rs.getString("nombre") +
+                        " - " + rs.getInt("edad") + ": " + rs.getString("contrato"));
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // apartado1();
+        apartado2();
     }
 }
