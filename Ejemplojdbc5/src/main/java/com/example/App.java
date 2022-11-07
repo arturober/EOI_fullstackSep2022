8bb0889c93fb714fe9dd6b78ab704bf18f27d9a2
Arturo Bernal <arturober@gmail.com>
2022-11-07 17:10:56 +0100

    Ejemplo transacciones cuenta bancaria

---
+++ Ejemplojdbc5/src/main/java/com/example/App.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public final static String BANCO = "jdbc:mariadb://localhost:3306/banco";
+    public final static String USER = "root";
+    public final static String PASS = "";
+
+    public static void transferirSaldo(int numOrigen, int numDestino, double cantidad) {
+        try (Connection conn = DriverManager.getConnection(BANCO, USER, PASS)) {
+            conn.setAutoCommit(false);
+            PreparedStatement stOrigen = conn.prepareStatement("UPDATE cuenta SET saldo = saldo - ? WHERE numero = ?");
+            stOrigen.setDouble(1, cantidad);
+            stOrigen.setInt(2, numOrigen);
+            int filasOrigen = stOrigen.executeUpdate();
+
+            if(filasOrigen == 1) {
+                PreparedStatement stDest = conn.prepareStatement("UPDATE cuenta SET saldo = saldo + ? WHERE numero = ?");
+                stDest.setDouble(1, cantidad);
+                stDest.setInt(2, numDestino);
+                int filasDest = stDest.executeUpdate();
+
+                if(filasDest == 1) {
+                    conn.commit(); // Confirmar transacción
+                } else {
+                    conn.rollback(); // Cancelar. No es necesario al cerrar la conexión en el try. Si no se cerrara, sí es necesario
+                }
+            } else {
+                conn.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        transferirSaldo(1001, 2003, 500);
+    }
+}
